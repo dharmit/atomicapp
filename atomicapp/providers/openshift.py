9a64d748f779ccd5ae9b924b6d102391d5e25451
Ratnadeep Debnath <rtnpro@gmail.com>
2015-12-16 18:52:19 +0530

    Refactored openshift provider for testing. #459
    - Move interaction with remote API to fetch resources to different methods, for easy mocking.

--- atomicapp/providers/openshift.py
+++ atomicapp/providers/openshift.py
@@ -55,35 +55,54 @@ class OpenShiftProvider(Provider):
             self._get_config_values()
 
         # construct full urls for api endpoints
-        self.openshift_api = urljoin(self.providerapi, "oapi/v1/")
         self.kubernetes_api = urljoin(self.providerapi, "api/v1/")
+        self.openshift_api = urljoin(self.providerapi, "oapi/v1/")
 
-        logger.debug("openshift_api = %s", self.openshift_api)
         logger.debug("kubernetes_api = %s", self.kubernetes_api)
+        logger.debug("openshift_api = %s", self.openshift_api)
 
+        self.oapi_resources = self._get_oapi_resources(self.providerapi)
+        self.kapi_resources = self._get_kapi_resources(self.providerapi)
+
+        self._process_artifacts()
+
+    def _get_kapi_resources(self):
+        """
+        Get kubernetes API resources
+        """
         # get list of supported resources for each api
-        (status_code, return_data) = self._make_request("get",
-                                                        self.openshift_api)
-        if status_code == 200:
-            self.oapi_resources = return_data["resources"]
-        else:
-            raise ProviderFailedException("Cannot get OpenShift resource list")
-
         (status_code, return_data) = self._make_request("get",
                                                         self.kubernetes_api)
         if status_code == 200:
-            self.kapi_resources = return_data["resources"]
+            kapi_resources = return_data["resources"]
         else:
             raise ProviderFailedException("Cannot get Kubernetes resource list")
 
         # convert resources list of dicts to list of names
-        self.oapi_resources = [res['name'] for res in self.oapi_resources]
-        self.kapi_resources = [res['name'] for res in self.kapi_resources]
+        kapi_resources = [res['name'] for res in kapi_resources]
 
-        logger.debug("Openshift resources %s", self.oapi_resources)
-        logger.debug("Kubernetes resources %s", self.kapi_resources)
+        logger.debug("Kubernetes resources %s", kapi_resources)
 
-        self._process_artifacts()
+        return kapi_resources
+
+    def _get_oapi_resources(self):
+        """
+        Get Openshift API resources
+        """
+        # get list of supported resources for each api
+        (status_code, return_data) = self._make_request("get",
+                                                        self.openshift_api)
+        if status_code == 200:
+            oapi_resources = return_data["resources"]
+        else:
+            raise ProviderFailedException("Cannot get OpenShift resource list")
+
+        # convert resources list of dicts to list of names
+        oapi_resources = [res['name'] for res in oapi_resources]
+
+        logger.debug("Openshift resources %s", oapi_resources)
+
+        return oapi_resources
 
     def _get_namespace(self, artifact):
         """
